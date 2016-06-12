b39332d2b72c4c5517f1950b5199bdc27728ae27
Malkollm <fatkullin@bk.ru>
2016-06-12 22:00:23 +0300

    Update Lab42.java

--- Lab4/src/lab4/Lab42.java
+++ Lab4/src/lab4/Lab42.java
@@ -10,24 +10,11 @@ import lab4.Lab4;
  */
 public class Lab42{
 
-    //выводит массив который создан в этом методе
-    public static int[] Output_result(int[]nums){
-        System.out.println("\n Массив выведенный через метод: ");
-
-        for (int i = 0; i < nums.length; i++) {
-            //массив = от 1 до 4 (рандомно)
-            nums[i] = (int) (Math.random() * 15);
-        }
-        return nums;
+    public static void main(String[] args){
+    int i=0;
+    while(i<5){        
+    System.out.println("mass" + Arrays.toString(Lab41.random_mass(1,4)));
+    i++;
     }
-
-    public static void main(String[] args) {
-        int[] arr = new int[10];
-        int i = 0; //используется в цикле для вывода 5и массивов
-        Output_result(arr); //вывод 1 массива
-        while (i<5){ //вывод 5и массивов заполненных в  предыдущей задаче
-            System.out.println("Массив " + Arrays.toString(Lab4.rand_mass(1,4))); //обращение к классу Main и методу rand_numb
-            i++;
-        }
     }
 }
